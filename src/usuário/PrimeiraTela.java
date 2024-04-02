b025ea227db532e16a90c2461d9cabdef43ffb50
guilherme26BC <logonrmlocal@fiap.com.br>
2024-04-02 11:17:17 -0300

    arrays

---
+++ src/usuário/PrimeiraTela.java
@@ -0,0 +1,94 @@
+package usuário;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+
+public class PrimeiraTela {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrimeiraTela window = new PrimeiraTela();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PrimeiraTela() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frame.setBounds(200, 200, 500, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{484, 0};
+		gridBagLayout.rowHeights = new int[]{21, 310, 23, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(189, 242, 240));
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setForeground(Color.BLACK);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.anchor = GridBagConstraints.NORTH;
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		frame.getContentPane().add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{226, 36, 0};
+		gbl_panel.rowHeights = new int[]{17, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Sair");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 2;
+		frame.getContentPane().add(btnNewButton, gbc_btnNewButton);
+	}
+
+}
---
+++ src/usuário/SegundaTela.java
@@ -0,0 +1,100 @@
+package usuário;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import java.awt.Color;
+
+public class SegundaTela {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SegundaTela window = new SegundaTela();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SegundaTela() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame("Teste");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+
+		// CRIAÇÃO e PROPRIEDADES DO BOTAO
+		JButton btnNewButton = new JButton("Sair");
+		btnNewButton.setBackground(SystemColor.window);
+		btnNewButton.setToolTipText("Clique aqui para sair da aplicação\r\n");
+		btnNewButton.setForeground(SystemColor.controlShadow);
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		btnNewButton.setBounds(334, 227, 90, 25);
+
+		btnNewButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		// ADICIONANDO O BOTÂO AO PAINEL
+		panel.add(btnNewButton);
+
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setBounds(20, 11, 46, 14);
+		panel.add(lblNewLabel);
+
+		textField = new JTextField();
+		textField.setBounds(20, 28, 389, 33);
+		panel.add(textField);
+		textField.setColumns(10);
+
+		JButton btnBemVindo = new JButton("Bem vindo");
+		btnBemVindo.setToolTipText("Clique aqui para sair da aplicação\r\n");
+		btnBemVindo.setForeground(SystemColor.controlShadow);
+		btnBemVindo.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		btnBemVindo.setBackground(Color.WHITE);
+		btnBemVindo.setBounds(213, 227, 102, 25);
+
+		btnBemVindo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String text = textField.getText();
+				JOptionPane.showMessageDialog(null, "bemVindo: " + text);
+			}
+		});
+		panel.add(btnBemVindo);
+	}
+}
