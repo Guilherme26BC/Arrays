781427d7d511f537ab6502ad17745a77141ba381
guilherme26BC <logonrmlocal@fiap.com.br>
2024-04-02 09:40:30 -0300

    Collections

--- src/usuário/Collection.java
+++ src/usuário/Collection.java
@@ -12,15 +12,15 @@ public class Collection {
 	static List<Usuario> list = new ArrayList<Usuario>();	//instancia do list como lista global para poder usar no método sem parametro
 	
 	public static void main(String[] args) {
-
+		
 		Collection test = new Collection(); //instanciar um objeto dentro da própria classe para poder usar o metodo remover
-	
+		//INICIALIZAÇÃO DOS USUARIOS
 		Usuario jade = new Usuario(31, "Jade", "1230");
 		Usuario ruby = new Usuario(32, "Ruby", "1230");
 		Usuario crystal = new Usuario(12, "Crystal", "1230");
 		
 		
-		
+		//COLOCANDO OS USUARIOS NA LISTA
 		test.list.add(jade);
 		test.list.add(ruby);
 		test.list.add(crystal);
@@ -28,21 +28,15 @@ public class Collection {
 			System.out.println("nome: " + usuario.getNome());
 		}
 		
+		//ORDENANDO A LISTA CONFORME O ID
 		System.out.println("Ordenando...");
 		Collections.sort(list);
 		for (Usuario usuario : test.list) {
 			System.out.println("nome: " + usuario.getNome() + "\tId:" + usuario.getId());
 		}
 	
-		Map mapa = new HashMap();
-		mapa.put("Crystal", crystal);
-		mapa.put("Ruby", ruby);
-		mapa.put("Jade", jade);
-
-		Object objeto = mapa.get("Jade");
-		Usuario usuario = (Usuario)objeto;
-		System.out.println(usuario.getNome());
 		
+		//PROCURAR UM NOME NA LISTA E REMOVE
 		List<Usuario> listaAtualizada = new ArrayList<Usuario>();
 		listaAtualizada = test.remover("Crystal");
 
@@ -50,9 +44,22 @@ public class Collection {
 			System.out.println(user.getNome());
 		}
 		
-		
-	}
+		//MAPA
+				Map mapa = new HashMap();
+				mapa.put(1, jade);
+				mapa.put(2, crystal);
+				mapa.put(3, ruby);
 
+				System.out.println("mapa");
+				
+				for(int i=1; i<=mapa.size(); i++) {
+				Object objeto = mapa.get(i);
+				Usuario usuario = (Usuario)objeto;
+				System.out.println("usuario: " + usuario.getNome());
+				}
+				System.out.println("--------------------");
+	}
+	//METODO PARA PROCURAR ALGUM NOME NA LISTA
 	public List<Usuario> remover(String nome) {
 		Iterator<Usuario> iterator = list.iterator();
 		while (iterator.hasNext()) {
